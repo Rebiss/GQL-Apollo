9a5a38d87b5a74459985f2ee3f34448ec5897b2a
Andranik Barseghyan <andranik.barseghyan@betconstruct.com>
2019-07-28 22:08:18 +0400

    add_all File

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './style/logo.jpg';
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      SpaceX
+      <div className='sap'>
+        <img src={logo} alt="SpaceX"/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
